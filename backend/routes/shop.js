03350a42694dda288fe26e860d19fecebc679420
Nitesh <Nitesh.e21@nsut.ac.in>
2023-11-05 19:02:49 +0530

    changes in addshop api

--- backend/routes/shop.js
+++ backend/routes/shop.js
@@ -1,9 +1,17 @@
 let router= require("express").Router();
+let shop= require("../model/shops");
 
-
+router.get("/",async(req,res,next)=>{
+    try {
+        let data = await shop.find();
+        res.send(data);
+    } catch (error) {
+        
+    }
+})
 router.post("/addshop",(req,res,next)=>{
-let {shopname,category,email,password}= req.query;
-let newShop=new shop(shopname,category,email,password);
+let {shopName,category,email,password}= req.body;
+let newShop=new shop({shopName,category,email,password});
 newShop.save();
 res.send("shop register successfully");
 })
